dfb8aef5f81a39008c295ba43eff8cb8beb19fbb
Stefan Blomberg <steblo94@gmail.com>
2017-10-18 16:21:46 +0200

    Stefan

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
-Const {Client} = require("pg"); 
-
-
-function greet () {
-    let greets = ["Hei!", "Hallo!", "Næmmen hallaien!", "Skjera Bagera?"];
-    let ran = greets[Math.floor((Math.random()*greets.length))];
-    return ran;
-}
+const {Client} = require("pg"); 
 
 app.set('port', (process.env.PORT || 8080));
+app.use(express.static('public'));
 
+client.connect();
 
-Let client = new Client ({ 
-ConnectionString:rocess.env.DATABASE_URL, 
-Ssl:true 
-}); 
+let client = new Client ({ 
+    ConnectionString:rocess.env.DATABASE_URL, 
+    //ssl:true 
+});     
+
 
 app.get('/ran/:id', function (req, res) {
   res.send(greet() + " " + req.params.id)
@@ -25,8 +20,34 @@ app.get('/ran/:id', function (req, res) {
 })
 
 app.listen(8080, function () {
-    console.log(greet() + " Christian")
     console.log('app listening on port 8080!')
     
 })
 
+
+
+
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 8080));
+
+
+app.use(express.static('/public'));
+
+function greet () {
+    let greets = ["Hei!", "Hallo!", "Næmmen hallaien!", "Skjera Bagera?"];
+    let ran = greets[Math.floor((Math.random()*greets.length))];
+    return ran;
+}
+
+//API
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+
+app.listen(app.get('port'), function() {
+    console.log(greet() + " Christian")
+    console.log('Koblet opp til port', app.get('port'));
+});
